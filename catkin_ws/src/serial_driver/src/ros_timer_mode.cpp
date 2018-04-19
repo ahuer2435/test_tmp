958d0d8237b63c24a07154e2ca0978ec3f7091ea
ahuer2435 <18221079843@139.com>
2018-04-19 19:31:12 +0800

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:add ros timer mode .
    	modified:   catkin_ws/src/serial_driver/CMakeLists.txt
    	new file:   catkin_ws/src/serial_driver/src/ros_timer_mode.cpp

---
+++ catkin_ws/src/serial_driver/src/ros_timer_mode.cpp
@@ -0,0 +1,48 @@
+#include <ros/ros.h>
+#include "serial_driver/serial_data.h"
+
+// global raspmsgsend
+
+ros::Publisher pub_msg1;
+
+static void callback1(const serial_driver::serial_data& global_input)
+{
+    //setting raspmsgsend by global_input
+}
+
+static void callback2(const serial_driver::serial_data& global_input)
+{
+    //setting raspmsgsend by global_input
+}
+
+
+static void callback3(const serial_driver::serial_data& global_input)
+{
+    //setting raspmsgsend by global_input
+}
+
+void timer_callback(const ros::TimerEvent&)
+{
+    serial_driver::serial_data pub_serial_data;
+    //setting pub_serial_data by raspmsgsend
+    pub_serial_data.id = "pass";
+    pub_serial_data.data = 0.1;
+    pub_msg1.publish(pub_serial_data);
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "ros_timer_mode_node");
+    ros::NodeHandle nh;
+
+    const double_t pub_freq = 10.0;
+
+    ros::Subscriber sub_msg1 = nh.subscribe("driver/from_serial1",10,callback1);
+    ros::Subscriber sub_msg2 = nh.subscribe("driver/from_serial2",10,callback2);
+    ros::Subscriber sub_msg3 = nh.subscribe("driver/from_serial3",10,callback3);
+    pub_msg1 = nh.advertise<serial_driver::serial_data>("driver/from_serial4",10,true);
+    ros::Timer timer1 = nh.createTimer(ros::Duration(1.0/pub_freq), timer_callback);
+
+    ros::spin();
+    return 0;
+}
