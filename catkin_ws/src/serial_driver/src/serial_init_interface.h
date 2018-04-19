f64bcb249a6bc7d0ebe87de620fde598d8e59b99
ahuer2435 <18221079843@139.com>
2018-04-19 20:35:15 +0800

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed: thin recevie nodes code.
    	modified:   catkin_ws/src/serial_driver/CMakeLists.txt
    	modified:   catkin_ws/src/serial_driver/src/serial_init_interface.h
    	modified:   catkin_ws/src/serial_driver/src/serial_receive_multi_topic_node.cpp
    	modified:   catkin_ws/src/serial_driver/src/serial_receive_node.cpp

--- catkin_ws/src/serial_driver/src/serial_init_interface.h
+++ catkin_ws/src/serial_driver/src/serial_init_interface.h
@@ -7,6 +7,77 @@ extern "C" {
 #define SIGSET    1
 #define SIGUNSET  0
 
+typedef enum{
+    min_state,
+    IDLE,
+    POWON,
+    RUNNING,
+    POWOFF,
+    ERROR,
+    max_state,
+}SYS_STATE;
+
+typedef enum{
+    MOTOIDLE = max_state,
+    MOTOINITIAL,//pown on
+    MOTOMODESET,
+    MOTOSETPARAM,
+    MOTOWAITSTARTSUCCESS,
+    MOTOSTART,
+    MOTOSTOP,   //emg stop
+    MOTORUNNING,
+    MOTORELAXMODE,
+
+    MOTOPOWOFF,
+    MOTOERROR,
+    motor_max_state,
+}MOTOR_STATE;
+
+typedef enum{
+    GAITIDLE = motor_max_state,
+    GAITINITIAL,
+    GAITSTART,
+    GAITSTOP,   //emg stop
+    GAITRUNNING,
+    GAITPOWOFF,
+    gait_max_state,
+}GAIT_STATE;
+
+
+typedef enum{
+    MCUIDLE = gait_max_state,
+    MCUINITIAL,
+    MCURUN,
+    MCUSTOP,
+    MCUPOWOFF,
+    MCUERROR,
+    mcu_max_state,
+}MCU_STATE;
+
+
+typedef enum
+{
+  ER_MOTORCRT = mcu_max_state, //mcu
+  ER_MOTOCNCT,
+  ER_MOTOINI ,
+  ER_GAIT1 ,
+  ER_GAIT2 ,
+  er_max_state,
+} ErrorMsgTypeDef;
+
+typedef enum{
+    FINDHEAD = er_max_state,
+    GETMSG,
+    FINDTAIL,
+    CHECKDATA,
+    PROCESSDATA,
+    tty_max_state,
+}TTY_STATE;
+
+
+#define SIGSET    1
+#define SIGUNSET  0
+
 #include "mcumsg_rasp.h"
 #include "thread_of_lcd.h"
 
--- catkin_ws/src/serial_driver/src/serial_receive_node.cpp
+++ catkin_ws/src/serial_driver/src/serial_receive_node.cpp
@@ -2,21 +2,7 @@
 #include "serial_driver/serial_data.h"
 #include "boost/thread.hpp"
 
-#include<fstream>
-
-#include <stdio.h>          //puts
-#include <string.h>         //strlen
-#include <stdlib.h>
-#include <unistd.h>         /*Unix 标准函数定义*/
-#include <sys/time.h>
-
-#include <assert.h>
-
-//for tty
-#include <termios.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include "serial_init_interface.h"
 
 #include "mcumsg_rasp.h"
 #include "thread_of_lcd.h"
@@ -26,130 +12,7 @@ static int gaitcalibrationflag ;
 static int motorcalibrationflag ;
 
 static RaspMsgSendTypeDef raspmsgsend;
-
-#define SIGSET    1
-#define SIGUNSET  0
-
-typedef enum{
-    min_state,
-    IDLE,
-    POWON,
-    RUNNING,
-    POWOFF,
-    ERROR,
-    max_state,
-}SYS_STATE;
-
-typedef enum{
-    MOTOIDLE = max_state,
-    MOTOINITIAL,//pown on
-    MOTOMODESET,
-    MOTOSETPARAM,
-    MOTOWAITSTARTSUCCESS,
-    MOTOSTART,
-    MOTOSTOP,   //emg stop
-    MOTORUNNING,
-    MOTORELAXMODE,
-
-    MOTOPOWOFF,
-    MOTOERROR,
-    motor_max_state,
-}MOTOR_STATE;
-
-typedef enum{
-    GAITIDLE = motor_max_state,
-    GAITINITIAL,
-    GAITSTART,
-    GAITSTOP,   //emg stop
-    GAITRUNNING,
-    GAITPOWOFF,
-    gait_max_state,
-}GAIT_STATE;
-
-
-typedef enum{
-    MCUIDLE = gait_max_state,
-    MCUINITIAL,
-    MCURUN,
-    MCUSTOP,
-    MCUPOWOFF,
-    MCUERROR,
-    mcu_max_state,
-}MCU_STATE;
-
-
-typedef enum
-{
-  ER_MOTORCRT = mcu_max_state, //mcu
-  ER_MOTOCNCT,
-  ER_MOTOINI ,
-  ER_GAIT1 ,
-  ER_GAIT2 ,
-  er_max_state,
-} ErrorMsgTypeDef;
-
-typedef enum{
-    FINDHEAD = er_max_state,
-    GETMSG,
-    FINDTAIL,
-    CHECKDATA,
-    PROCESSDATA,
-    tty_max_state,
-}TTY_STATE;
-
-
 ros::Publisher pub_msg1;
-
-static int serial_init()
-{
-    fd = open("/dev/ttyUSB0",O_RDWR );
-    if(-1 == fd){
-        close(fd);
-        printf("UBUNTU open /dev/ttyUSB0 error!\n");
-        exit(0);
-    }
-    printf("Mcu_Recv open /dev/ttyUSB0 success!\n");
-    sleep(1);
-
-    struct termios opt;
-    tcgetattr(fd, &opt);
-    cfsetispeed(&opt, B115200);
-    cfsetospeed(&opt, B115200);
-    if(tcsetattr(fd, TCSANOW, &opt) != 0 )
-    {
-        perror("tcsetattr error");
-        return -1;
-    }
-
-    opt.c_cflag &= ~CSIZE;
-    opt.c_cflag |= CS8;
-    opt.c_cflag &= ~CSTOPB;
-    opt.c_cflag &= ~PARENB;
-    opt.c_cflag &= ~INPCK;
-    opt.c_cflag |= (CLOCAL | CREAD);
-
-    opt.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-
-    opt.c_oflag &= ~OPOST;
-    opt.c_oflag &= ~(ONLCR | OCRNL);    //添加的
-
-    opt.c_iflag &= ~(ICRNL | INLCR);
-    opt.c_iflag &= ~(IXON | IXOFF | IXANY);    //添加的
-
-    opt.c_cc[VTIME] = 1;
-    opt.c_cc[VMIN] = 0;
-
-    tcflush(fd, TCIOFLUSH);
-
-    printf("mcu_recv ttyUSB0 configure complete\n");
-
-    if(tcsetattr(fd, TCSANOW, &opt) != 0)
-    {
-        perror("serial error");
-        return -1;
-    }
-}
-
 
 void thread_of_mcu_recv(void){
 
@@ -188,6 +51,7 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "serial1_receive_node");
     ros::NodeHandle nh;
+    fd = serial_init();
 
     pub_msg1 = nh.advertise<serial_driver::serial_data>("driver/from_serial1",10,true);
     boost::thread reveive_loop(&receive_loop_msg);
